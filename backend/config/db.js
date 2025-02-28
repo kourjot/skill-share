c06c092add7c2e16ebcf2b69a07510772c52cade
kourjot <pkour6188@gmail.com>
2025-03-01 00:29:26 +0530

    second commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import {connect} from "mongoose"
+import "dotenv/config"
+const connection =async()=>{
+    try{
+        await connect("mongodb+srv://pkour6188:WJGKzN2xyxM4RYjN@skill-share.oflnc.mongodb.net/?retryWrites=true&w=majority&appName=skill-share")
+        console.log("Connected to MongoDB")
+    }catch(err){
+        console.error("Failed to connect to MongoDB", err)
+    }
+}
+export {connection}
