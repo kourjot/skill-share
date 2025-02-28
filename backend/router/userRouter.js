63c8deb6bc9aed6ebaa3c0826a2a65b913d482fe
vijaynimar <vijaynimar8@gmail.com>
2025-03-01 03:01:48 +0530

    profile

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -2,9 +2,10 @@ import Router from "express"
 import {login,registration} from "../controller/userController.js"
 import {sendRegisterOtp} from "../controller/nodemailerController.js"
 import { profileMulter } from "../middleware/multer.js"
+import { profileUpload } from "../controller/userProfile.js"
 const userRouter = Router()
 userRouter.post("/otp-register",sendRegisterOtp)
 userRouter.post("/sign-in",registration)
 userRouter.post("/log-in",login)
-// userRouter.post("/profile",profile)
+userRouter.post("/profile",profileMulter,profileUpload)
 export {userRouter}
