212b0c38ac837f0f27a8755fc779968bdf8beff0
vijaynimar <vijaynimar8@gmail.com>
2025-03-01 00:48:32 +0530

    mongo connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
-import {connect} from "mongoose"
-import "dotenv/config"
-const connection =async()=>{
-    try{
-        await connect("mongodb+srv://pkour6188:WJGKzN2xyxM4RYjN@skill-share.oflnc.mongodb.net/?retryWrites=true&w=majority&appName=skill-share")
-        console.log("Connected to MongoDB")
-    }catch(err){
-        console.error("Failed to connect to MongoDB", err)
+import { connect } from "mongoose";
+import "dotenv/config";
+
+const connection = async () => {
+    try {
+        await connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
     }
-}
-export {connection}
+};
+
+export { connection };
