3c1d895a7e3c21c8935b360c0b6b27042323e0f7
vijaynimar <vijaynimar8@gmail.com>
2025-03-01 01:44:03 +0530

    schemaprofile

---
+++ backend/model/userProfile.js
@@ -0,0 +1,13 @@
+import {Schema,model} from "mongoose"
+const profileSchema=new Schema({
+    userId:{type:Schema.Types.ObjectId,ref:"user",required:true},
+    image:{type:String,default:""},
+    skills:[String],
+    description:{type:String},
+    totalFollower:{Type:Number,default:0},
+    totalFollows:{Type:Number,default:0},
+    follwers:[Number],
+    follows:[Number]
+})
+const profile=model("profile",profileSchema)
+export {profile}
