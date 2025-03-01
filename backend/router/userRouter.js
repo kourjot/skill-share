feabb764651a12cb24f1e36d68bdf61cf4e23702
kourjot <pkour6188@gmail.com>
2025-03-01 13:08:06 +0530

    Reset Password Controller

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -8,12 +8,15 @@ import { profileMulter } from "../middleware/multer.js"
 import { profileUpload,getProfile} from "../controller/userProfile.js"
 import  {tokenVerify} from "../middleware/tokenMiddleware.js"
 import  {forgotPassword} from "../controller/forgotPasswordController.js" 
+import {resetpassword} from "../controller/resetPasswordController.js"
 const userRouter = Router()
 
 userRouter.post("/otp-register",sendRegisterOtp)
 userRouter.post("/sign-in",registration)
 userRouter.post("/log-in",login)
 userRouter.post("/forgotPassword",forgotPassword)
+userRouter.post("/resetPassword",resetpassword)
 userRouter.post("/profile",profileMulter.single("image"),profileUpload)
 userRouter.get("/getprofile",tokenVerify,getProfile)
+
 export {userRouter}
