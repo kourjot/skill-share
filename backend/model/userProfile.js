b45541dcd79edeb56c5b7dc2a1b050af1169e250
vijaynimar <vijaynimar8@gmail.com>
2025-03-01 13:41:02 +0530

    updateProfile

--- backend/model/userProfile.js
+++ backend/model/userProfile.js
@@ -3,7 +3,7 @@ const profileSchema=new Schema({
     userId:{type:Schema.Types.ObjectId,ref:"user",required:true},
     image:{type:String,default:""},
     username:{type:String,unique:true,required:true},
-    skills:[String],
+    skills:{type:String},
     description:{type:String},
     totalFollower:{type:Number,default:0},
     totalFollows:{type:Number,default:0},
