ade205d2aec603b94415328a92579d8567c9a31a
kourjot <pkour6188@gmail.com>
2025-03-01 12:40:14 +0530

    All Image Model

---
+++ backend/model/allImagesModel.js
@@ -0,0 +1,10 @@
+import { Schema,model } from "mongoose";
+
+const AllImages=new Schema({
+    userId:{type:Schema.Types.ObjectId,ref:"user"},
+    username:{type:String},
+    images:[String]
+})
+
+const Images=model("Images",AllImages)
+export { Images}
