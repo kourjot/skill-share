5977b48bdb7fcce95f11cf987589b9dd4421197f
Ritik-Dethliya <dethliya73@gmail.com>
2025-03-01 22:50:28 +0530

    fetch the posts and display

--- frontend/skill-share/src/Components/DisplayPost.jsx
+++ frontend/skill-share/src/Components/DisplayPost.jsx
@@ -1,63 +1,58 @@
-import '../Styles/DisplayPost.css'
+import { useContext } from 'react';
+import { PostDataContext } from '@/ContextApi/PostData';
+import '../Styles/DisplayPost.css';
+
 function DisplayPost() {
-    const postdata=[{ user:"ritik", title:"there is am using to Capture the nature", 
-        description:"NAture love us we also need to love nature", hashtag:"Nature", 
-        images:["https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600",
-        "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600"
-        ],
-        likes:100
-        },
-        {user:"vijay", title:"there is am using to Capture the nature", 
-          description:"NAture love us we also need to love nature", hashtag:"Nature", 
-          images:["https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600",
-          "https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600"
-        ],
-        likes:200
-        },
-        {user:"vijay", title:"there is am using to Capture the nature", 
-          description:"NAture love us we also need to love nature", hashtag:"Nature", 
-          images:["https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&w=600",
-          "https://images.pexels.com/photos/1386604/pexels-photo-1386604.jpeg?auto=compress&cs=tinysrgb&w=600"
-        ],
-        likes:129
-        }
-      ]
-    return (
-      <>
-      <div className="displaypostcontainer">
-        {postdata.map((post)=>{
-          return(
-            <>
-              <div className="post">
-              <div className="post-header">
+  const [postdata, setPostdata] = useContext(PostDataContext);
+
+  console.log('Post Data Length:', Array.isArray(postdata) ? postdata.length : 'Not an array');
+
+  return (
+    <div className="displaypostcontainer">
+      {Array.isArray(postdata) && postdata.length > 0 ? (
+        postdata.map((post, index) => (
+          <div key={index} className="post">
+            <div className="post-header">
               <div className="avatar"></div>
               <div>
-              <h2 className="username">{post.user}</h2>
-              <p className="user-role">Aspiring Full Stack Developer</p>
+                <h2 className="username">{post.username}</h2>
+                <p className="user-role">Aspiring Full Stack Developer</p>
               </div>
-          </div>
-          <p className="post-title">{post.title}</p>
-          <p className="post-description">{post.description}</p>
-          <p className="post-hashtag">{post.hashtag}</p>
-          <div className="post-images">
-              {post.images.map((img, index) => (
-              <img key={index} src={img} alt="Post screenshot" className="post-image" style={{maxHeight:"200px"}}/>
-              ))}
-          </div>
-          <div className="post-actions">
+            </div>
+            <p className="post-title">{post.title}</p>
+            <p className="post-description">{post.description}</p>
+            <p className="post-hashtag">{post.hashtag}</p>
+            <div className="post-images">
+              {/* {post.images.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt="Post screenshot"
+                  className="post-image"
+                  style={{ maxHeight: '200px' }}
+                />
+              ))} */}
+              <img
+                  key={index}
+                  src={post.photo}
+                  alt="Post screenshot"
+                  className="post-image"
+                  style={{ maxHeight: '200px' }}
+                />
+            </div>
+            <div className="post-actions">
               <span>👍 {post.likes}</span>
               <span>💬 Comment</span>
               <span>🔄 Repost</span>
               <span>📤 Send</span>
+            </div>
           </div>
-          </div>
-            </>
-          )
-        }) 
-      }
-      </div>
-      </>
-    );
+        ))
+      ) : (
+        <p>No posts available.</p>
+      )}
+    </div>
+  );
 }
 
 export default DisplayPost;
