6f97257e747204b1279af1460f6d5bc46de2ba6a
vijaynimar <vijaynimar8@gmail.com>
2025-03-02 15:31:17 +0530

    community

--- backend/model/community.js
+++ backend/model/community.js
@@ -3,7 +3,7 @@ const communitySchema=new mongoose.Schema({
     userId:{
         type:mongoose.Schema.Types.ObjectId,ref:"user"
     },
-    name:{type:String},
+    name:{type:String,unique:true,required:true},
     username:{type:String,required:true},
     members:[{image:String,username:String}],
     messages:[{image:String,username:String,messages:String}]
