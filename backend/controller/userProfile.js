5578382c5067cce06c8d611094693dd12e992bed
vijaynimar <vijaynimar8@gmail.com>
2025-04-08 11:34:07 +0530

    profile

--- backend/controller/userProfile.js
+++ backend/controller/userProfile.js
@@ -33,8 +33,7 @@ const profileUpload = async (req, res) => {
           const { email,username } = decoded;
         // console.log(email,username)
           // Check if the user exists in the database
-          const userExist = await user.find
-          One({ email });
+          const userExist = await user.findOne({ email });
           if (!userExist) {
               return res.status(400).json({ message: "User does not exist" });
           }
