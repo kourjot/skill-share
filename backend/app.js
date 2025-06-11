0dd5308d3e954a15c6a961e746c1f7d803cc8e5d
vijaynimar <vijaynimar8@gmail.com>
2025-06-11 12:29:34 +0530

    update code

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import cron from "node-cron"
 import passport from "./config/passport.js"; 
 import { connection } from "./config/db.js"
 
@@ -12,11 +11,7 @@ const port = process.env.PORT ||3110
 const app = express();
 app.use(cors())
 app.use(express.json());
-cron.schedule('*/14 * * * *',async()=>{
-    const data=await fetch("https://skill-share-1-v5lq.onrender.com/")
-    let asyncData=await data.json()
-    console.log(asyncData)
-})
+
 app.use(passport.initialize());
 app.use(userRouter)
 app.listen(port,()=>{
